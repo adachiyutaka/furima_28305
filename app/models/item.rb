d5984a745c6ee8feaff636609d177114b1461c87
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-29 19:54:07 +0900

    userモデルにアクティブハッシュ用のアソシエーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   with_options presence: true do
     validates :name
     validates :text
-    validates :category
-    validates :condition
-    validates :including_postage
-    validates :consignor_location
-    validates :ready_time
+    validates :category_id
+    validates :condition_id
+    validates :including_postage_id
+    validates :consignor_location_id
+    validates :ready_time_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
   end
 
   has_one_attached :image
+  belongs_to_active_hash :category_id, :condition_id, :including_postage_id, :consignor_location_id, :ready_time_id
   belongs_to :user
 end
