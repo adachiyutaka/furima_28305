4493c281cc0a3a52110314ae35221cc0e5ea4b17
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-30 19:53:53 +0900

    新規Itemモデルの保存処理を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
   end
   
   private
-  def message_params
+  def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :including_postage_id, :consignor_location_id, :ready_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
