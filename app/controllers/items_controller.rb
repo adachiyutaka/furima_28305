386b78abe44fda17f30b87658d6bc4a3c44f4ebc
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-30 14:05:50 +0900

    商品出品ページのアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,16 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
   
+  private
+  def message_params
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :including_postage_id, :consignor_location_id, :ready_time_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
