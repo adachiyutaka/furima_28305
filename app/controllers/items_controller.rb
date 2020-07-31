6481bf04adcd0456c2f609df208937d3268f33f1
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-31 15:24:34 +0900

    商品詳細表示のためのアクションとルーティングを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,11 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order(id: "DESC")
+  end
+
+  def show
   end
 
   def new
