e10debd5047ee1143938378cb2e2191e68692953
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-31 09:59:36 +0900

    Itemモデルのバリデーションに抜けがあったため追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   with_options presence: true do
-    validates :name
-    validates :text
+    validates :name,  length: { maximum: 40 } 
+    validates :text,  length: { maximum: 1000 } 
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     with_options numericality: { other_than: 1 } do
       validates :category_id
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
       validates :ready_time_id
     end
   end
+  validate :image_presence
 
   has_one_attached :image
   belongs_to_active_hash :category
@@ -21,4 +22,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :consignor_location
   belongs_to_active_hash :ready_time
   belongs_to :user
+
+  def image_presence
+    errors.add(:image, 'must be attached') unless image.attached?
+  end
 end
